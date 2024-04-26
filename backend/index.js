55b7e6f7c705848c8f0043fcbf6cb38e0291f041
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-04-26 17:55:28 +0300

    router

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-require('./models/db')
+require("./models/db");
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
-
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 app.listen(PORT, () => {
   console.log(`connecting server on ${PORT}`);
 });
-
