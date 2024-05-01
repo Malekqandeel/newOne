36384c9380c6c8ab921baa442ae3d8d20a617302
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-05-01 09:41:41 +0300

    change package bcrypt to bcryptjs

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { register, login } = require("../controller/users");
 
 const usersRouter = express.Router();
-usersRouter.post("/register ", register);
+usersRouter.post("/register", register);
 usersRouter.post("/login", login);
 module.exports = usersRouter;
