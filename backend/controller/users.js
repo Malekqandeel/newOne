b3419dbfcf12c4f0d8b8b92f0f5fa7521ec555d4
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-05-03 16:24:16 +0300

    update and select users  by id

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -3,20 +3,15 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const register = async (req, res) => {
   const { username, password } = req.body;
-  const bcryptPassword = await bcrypt.hash(password, 7);
-
-  const query = "INSERT INTO users (username, password) VALUES (?, ?)";
-  const data = [username.toLowerCase(), bcryptPassword];
-  connection
-    .query(query, data)
-    .then((result) => {
-      res.status(201).json({
-        message: "register"
-      });
-    })
-    .catch((err) => {
-      throw err;
-    });
+  try {
+    const bcryptPassword = await bcrypt.hash(password, 7);
+    const query = "INSERT INTO users (username, password) VALUES (?, ?)";
+    const data = [username.toLowerCase(), bcryptPassword];
+    const result = await connection.query(query, data);
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to register user" });
+  }
 };
 const login = (req, res) => {
   const password = req.body.password;
@@ -64,4 +59,35 @@ const login = (req, res) => {
       });
     });
 };
-module.exports = { register, login };
+const getUserById = (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT * FROM users WHERE id= ?`;
+  connection
+    .query(query, id)
+    .then((result) => {
+      res.status(200).json({
+        message: `users id = ${id}`,
+        result: result
+      });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
+const updateUserById = (req, res) => {
+  const { id } = req.params;
+  const { username, password, type } = req.body;
+  const query = `UPDATE users SET username=? ,password =?,type=? WHERE id= ?`;
+  const data = [username, password, type, id];
+  connection
+    .query(query, data)
+    .then((result) => {
+      res.status(202).json({
+        message: `Modified user id =${id}  `
+      });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
+module.exports = { register, login, getUserById };
