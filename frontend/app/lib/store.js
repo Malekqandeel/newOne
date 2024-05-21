f302f0d95398b8e35a995f6c0ebabf5acc2fa174
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-05-21 23:55:47 +0300

    auth and users slice

--- frontend/app/lib/store.js
+++ frontend/app/lib/store.js
@@ -1,9 +1,11 @@
 "use client";
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/userSlice";
+import authReducer from "./features/auth";
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    auth: authReducer
   }
 });
 // export const store = () => {
