2476608efffb2ee8a067039eef082b492e93b8fc
Malekqandeel <malikqan@gmail.com>
2024-05-21 23:41:12 +0300

    testing

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,8 +1,10 @@
-export default function about() {
+export default function Home() {
     return (
       <>
         <span className="bg-auto">
-          <img src="https://images.unsplash.com/photo-1574169208507-84376144848b?q=80&w=2079&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          <h1>
+            home
+          </h1>
         </span>
       </>
     );
