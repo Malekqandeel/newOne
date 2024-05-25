aa5bdafd741ba93f9daf0da25a5bf55a779a9f9b
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-05-25 23:37:41 +0300

    check slice and edit login logic

--- frontend/app/StoreProvider.js
+++ frontend/app/StoreProvider.js
@@ -1,13 +1,13 @@
 "use client";
 import { useRef } from "react";
 import { Provider } from "react-redux";
-import { store } from "../lib/store";
+import { store } from "./lib/store";
 
 export default function StoreProvider({ children }) {
-  // const storeRef = useRef();
-  // if (!storeRef.current) {
-  //   storeRef.current = store();
-  // }
+  const storeRef = useRef();
+  if (!storeRef.current) {
+    storeRef.current = store;
+  }
 
   return <Provider store={store}>{children}</Provider>;
 }
